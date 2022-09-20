f8ee48f495bd56c38b87553adc30a7da51910f9e
aabdrakhman <abdrahman.aygerim@gmail.com>
2022-09-20 18:02:37 +0600

    First release

---
+++ botforgroup.py
@@ -0,0 +1,68 @@
+"""This is BotForGroup"""
+
+import logging
+
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ContentTypes, Message
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+from sqlite_db import DBHelper
+db = DBHelper()
+
+import asyncio
+import aioschedule
+
+API_TOKEN = str(os.getenv("API_TOKEN"))
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+@dp.message_handler(commands=['start', 'help'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` or `/help` command
+    """
+    await message.reply("Hi!\nI'm BotForGroup!")
+
+
+  
+@dp.message_handler()
+async def send_warning_message():
+    if len(db.warning_message()):
+        await bot.send_message(-759201949, ' , '.join(db.warning_message())+' За неделю от вас не было сообщений')
+    
+async def scheduler():
+    aioschedule.every().day.at("08:00").do(send_warning_message)
+    aioschedule.every().day.at("08:00").do(kick_member)
+    while True:
+        await aioschedule.run_pending()
+        await asyncio.sleep(1)
+
+async def on_startup(dp): 
+    asyncio.create_task(scheduler())
+
+
+ 
+@dp.message_handler()
+async def kick_member():
+    if len(db.kick_member_query()):
+        for i in db.kick_member_query():
+            await bot.kick_chat_member(-759201949, i)
+
+
+    
+@dp.message_handler()
+async def get_message(message: types.Message):
+    db.insert_message(message)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
---
+++ sqlite_db.py
@@ -0,0 +1,42 @@
+import sqlite3 as sql
+
+
+class DBHelper:
+
+    def insert_message(self, message):
+        conn = sql.connect('botgroup.db')
+        cur = conn.cursor()
+        cur.execute(f'SELECT user_id FROM users WHERE user_id={message.from_user.id}')
+        data = cur.fetchone()
+        if data is None:  
+            cur.execute(f'INSERT INTO users VALUES("{message.from_user.id}", "{message.from_user.username}", "{message.text}", "{message.date}")')
+            conn.commit()
+        else:
+            cur.execute("UPDATE users SET last_message = ?, last_message_date = ? WHERE user_id = ? ", (message.text, message.date, message.from_user.id))
+            conn.commit()
+
+
+ 
+    def warning_message(*args):
+        conn = sql.connect('botgroup.db')
+        cur = conn.cursor()
+        cur.execute("SELECT chatuser.user_name, strftime('%d-%m-%Y', 'now')-strftime('%d-%m-%Y', last_message_date) AS day FROM chatuser LEFT JOIN users USING(user_id) WHERE day>6")
+        row = cur.fetchall()
+        conn.commit()
+        mass = []
+        for i in row:
+            mass.append("@"+i[0])
+        return mass
+  
+  
+            
+    def kick_member_query(*args):
+        conn = sql.connect('botgroup.db')
+        cur = conn.cursor()
+        cur.execute("SELECT chatuser.user_id, strftime('%d-%m-%Y', 'now')-strftime('%d-%m-%Y', last_message_date) AS day FROM chatuser LEFT JOIN users USING(user_id) WHERE day>9")
+        row = cur.fetchall()
+        conn.commit()
+        mass = []
+        for i in row:
+            mass.append(i[0])
+        return mass
