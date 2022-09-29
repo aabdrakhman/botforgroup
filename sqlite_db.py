029135e6da5cd977d78ae997aa0219678c2ef8d8
aabdrakhman <abdrahman.aygerim@gmail.com>
2022-09-30 01:04:36 +0600

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -13,7 +13,7 @@ class DBHelper:
         cur.execute(sql_query, (message.from_user.id))
         data = cur.fetchone()
         if data is None:
-            sql_insert = """INSERT INTO users(user_id, user_name, message_t, last_message_date) VALUES(%s, %s, %s, %s)"""
+            sql_insert = "INSERT INTO users(user_id, user_name, message_t, last_message_date) VALUES(%s, %s, %s, %s)"
             cur.execute(sql_insert,(message.from_user.id, message.from_user.username, message.text, message.date))
             conn.commit()
         else:
