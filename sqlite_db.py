02a75d7de4a664720e62eeee3ae0196e92d7887b
aabdrakhman <abdrahman.aygerim@gmail.com>
2022-09-30 01:16:31 +0600

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -9,16 +9,13 @@ class DBHelper:
     def insert_message(self, message):
         conn = psycopg2.connect(DATABASE_URL, sslmode="require")
         cur = conn.cursor()
-        sql_query = 'SELECT user_id FROM users WHERE user_id= %s'
-        cur.execute(sql_query, (message.from_user.id))
+        cur.execute('SELECT user_id FROM users WHERE user_id= ?', (message.from_user.id))
         data = cur.fetchone()
         if data is None:
-            sql_insert = 'INSERT INTO users(user_id, user_name, message_t, last_message_date) VALUES(%s, %s, %s, %s)'
-            cur.execute(sql_insert, (message.from_user.id, message.from_user.username, message.text, message.date))
+            cur.execute('INSERT INTO users(user_id, user_name, message_t, last_message_date) VALUES(%s, %s, %s, %s)', (message.from_user.id, message.from_user.username, message.text, message.date))
             conn.commit()
         else:
-            sql_update = 'UPDATE users SET last_message = %s, last_message_date = %s WHERE user_id = %s'
-            cur.execute(sql_update, (message.text, message.date, message.from_user.id))
+            cur.execute('UPDATE users SET last_message = ?, last_message_date = ? WHERE user_id = ?', (message.text, message.date, message.from_user.id))
             conn.commit()
 
 
