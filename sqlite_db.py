9d0811aa1e4823a46a6e4199b47ad79a80d95037
aabdrakhman <abdrahman.aygerim@gmail.com>
2022-09-30 01:14:31 +0600

    Update sqlite_db.py

--- sqlite_db.py
+++ sqlite_db.py
@@ -10,7 +10,7 @@ class DBHelper:
         conn = psycopg2.connect(DATABASE_URL, sslmode="require")
         cur = conn.cursor()
         sql_query = 'SELECT user_id FROM users WHERE user_id= %s'
-        cur.execute(sql_query, (message.from_user.id,))
+        cur.execute(sql_query, (message.from_user.id))
         data = cur.fetchone()
         if data is None:
             sql_insert = 'INSERT INTO users(user_id, user_name, message_t, last_message_date) VALUES(%s, %s, %s, %s)'
@@ -18,7 +18,7 @@ class DBHelper:
             conn.commit()
         else:
             sql_update = 'UPDATE users SET last_message = %s, last_message_date = %s WHERE user_id = %s'
-            cur.execute(sql_update, (message.text, message.date, message.from_user.id,))
+            cur.execute(sql_update, (message.text, message.date, message.from_user.id))
             conn.commit()
 
 
